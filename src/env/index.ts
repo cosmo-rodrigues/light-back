cab8ceb9cd2e2f85454066db808943dc122acde3
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2023-12-23 17:02:51 -0300

    feat(database) - Database created

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,11 +4,11 @@ import { z } from 'zod';
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(30000),
-  DB_PASSWORD: z.string(),
   DB_USER: z.string(),
-  DB_NAME: z.string(),
-  DB_PORT: z.string(),
+  DB_PASSWORD: z.string(),
   DB_HOST: z.string(),
+  DB_PORT: z.string(),
+  DB_NAME: z.string(),
 });
 
 const _parsedEnv = envSchema.safeParse(process.env);
