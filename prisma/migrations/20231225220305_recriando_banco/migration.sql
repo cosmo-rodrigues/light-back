0fdaf183e98f920dad92368b1d2f3f34de4c66b3
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2023-12-25 19:45:41 -0300

    refactor(database) - Changed db structure

--- prisma/migrations/20231225220305_recriando_banco/migration.sql
+++ prisma/migrations/20231225220305_recriando_banco/migration.sql
@@ -5,6 +5,7 @@ CREATE TYPE "State" AS ENUM ('AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO
 CREATE TABLE "clientes" (
     "id" SERIAL NOT NULL,
     "nome" TEXT NOT NULL,
+    "numeroDoCliente" TEXT NOT NULL,
 
     CONSTRAINT "clientes_pkey" PRIMARY KEY ("id")
 );
@@ -13,11 +14,13 @@ CREATE TABLE "clientes" (
 CREATE TABLE "enderecos" (
     "id" SERIAL NOT NULL,
     "endereco" TEXT NOT NULL,
-    "numero" INTEGER,
+    "numero" TEXT,
     "sem_numero" BOOLEAN NOT NULL DEFAULT true,
+    "complemento" TEXT,
     "bairro" TEXT NOT NULL,
     "cidade" TEXT NOT NULL,
     "estado" "State" NOT NULL,
+    "cep" TEXT,
     "cliente_id" INTEGER,
 
     CONSTRAINT "enderecos_pkey" PRIMARY KEY ("id")
@@ -26,6 +29,7 @@ CREATE TABLE "enderecos" (
 -- CreateTable
 CREATE TABLE "instalacoes" (
     "id" SERIAL NOT NULL,
+    "numeroDaInstalacao" TEXT NOT NULL,
     "cliente_id" INTEGER NOT NULL,
     "endereco_id" INTEGER,
 
@@ -33,29 +37,30 @@ CREATE TABLE "instalacoes" (
 );
 
 -- CreateTable
-CREATE TABLE "contas" (
+CREATE TABLE "faturas" (
     "id" SERIAL NOT NULL,
     "endereco_id" INTEGER NOT NULL,
-    "instalacaoId" INTEGER NOT NULL,
+    "instalacao_id" INTEGER NOT NULL,
     "data_da_emissao" TIMESTAMP(3) NOT NULL,
     "data_do_vencimento" TIMESTAMP(3) NOT NULL,
+    "fatura_paga" BOOLEAN NOT NULL,
     "data_da_leitura_anterior" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "data_da_leitura_atual" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "data_da_proxima_leitura" TIMESTAMP(3) NOT NULL DEFAULT now() + interval '1 month',
-    "energia_eletrica_em_KWh" INTEGER NOT NULL DEFAULT 0,
-    "energia_eletrica_consumo_total" INTEGER NOT NULL DEFAULT 0,
-    "energia_eletrica_valor_unitario" DOUBLE PRECISION NOT NULL DEFAULT 0,
-    "energia_eletrica_valor_total" DOUBLE PRECISION NOT NULL DEFAULT 0,
-    "energia_SCEE_sem_ICMS_em_KWh" INTEGER NOT NULL DEFAULT 0,
-    "energia_SCEE_sem_ICMS_consumo_total" INTEGER NOT NULL DEFAULT 0,
-    "energia_SCEE_sem_ICMS_valor_unitario" DOUBLE PRECISION NOT NULL DEFAULT 0,
-    "energia_SCEE_sem_ICMS_valor_total" DOUBLE PRECISION NOT NULL DEFAULT 0,
-    "energia_compensada_GDI_em_KWh" INTEGER NOT NULL DEFAULT 0,
-    "energia_compensada_GDI_gerada" INTEGER NOT NULL DEFAULT 0,
-    "energia_compensada_GDI_valor_unitario" DOUBLE PRECISION NOT NULL DEFAULT 0,
-    "energia_compensada_GDI_valor_total" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "energia_eletrica_quantidade" TEXT NOT NULL,
+    "energia_eletrica_preco_unitario" TEXT NOT NULL,
+    "energia_eletrica_valor_total" TEXT NOT NULL,
+    "energia_eletrica_tarifa_unitaria" TEXT NOT NULL,
+    "energia_SCEE_sem_ICMS_quantidade" TEXT NOT NULL,
+    "energia_SCEE_sem_ICMS_preco_unitario" TEXT NOT NULL,
+    "energia_SCEE_sem_ICMS_valor_total" TEXT NOT NULL,
+    "energia_SCEE_sem_ICMS_tarifa_unitaria" TEXT NOT NULL,
+    "energia_compensada_GDI_quantidade" TEXT NOT NULL,
+    "energia_compensada_GDI_preco_unitario" TEXT NOT NULL,
+    "energia_compensada_GDI_valor_total" TEXT NOT NULL,
+    "energia_compensada_GDI_tarifa_unitaria" TEXT NOT NULL,
 
-    CONSTRAINT "contas_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "faturas_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -68,7 +73,7 @@ CREATE UNIQUE INDEX "enderecos_id_key" ON "enderecos"("id");
 CREATE UNIQUE INDEX "instalacoes_id_key" ON "instalacoes"("id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "contas_id_key" ON "contas"("id");
+CREATE UNIQUE INDEX "faturas_id_key" ON "faturas"("id");
 
 -- AddForeignKey
 ALTER TABLE "enderecos" ADD CONSTRAINT "enderecos_cliente_id_fkey" FOREIGN KEY ("cliente_id") REFERENCES "clientes"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -80,7 +85,7 @@ ALTER TABLE "instalacoes" ADD CONSTRAINT "instalacoes_cliente_id_fkey" FOREIGN K
 ALTER TABLE "instalacoes" ADD CONSTRAINT "instalacoes_endereco_id_fkey" FOREIGN KEY ("endereco_id") REFERENCES "enderecos"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "contas" ADD CONSTRAINT "contas_endereco_id_fkey" FOREIGN KEY ("endereco_id") REFERENCES "enderecos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "faturas" ADD CONSTRAINT "faturas_endereco_id_fkey" FOREIGN KEY ("endereco_id") REFERENCES "enderecos"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "contas" ADD CONSTRAINT "contas_instalacaoId_fkey" FOREIGN KEY ("instalacaoId") REFERENCES "instalacoes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "faturas" ADD CONSTRAINT "faturas_instalacao_id_fkey" FOREIGN KEY ("instalacao_id") REFERENCES "instalacoes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
